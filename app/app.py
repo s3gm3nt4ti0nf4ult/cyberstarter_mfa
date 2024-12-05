8c46f898ff5df610199a02da08fff9f917dc8e7d
s3gm3nt4ti0nf4ult <foxtrot@foxtrotlabs.cc>
2024-12-05 22:54:28 +0100

    only localhost, nginx fix

--- app/app.py
+++ app/app.py
@@ -103,4 +103,4 @@ def download_qr():
     return send_file(buffer, as_attachment=True, download_name="otp_qr.png", mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
